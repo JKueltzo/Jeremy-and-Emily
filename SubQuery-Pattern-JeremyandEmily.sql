5619a91e42934ad89d1e0e717474853349815af9
JKueltzo <54907271+JKueltzo@users.noreply.github.com>
2019-10-30 12:05:38 -0500

    Create  SubQuery-Pattern-JeremyandEmily.sql

---
+++ SubQuery-Pattern-JeremyandEmily.sql
@@ -0,0 +1,23 @@
+SELECT StartDate, EndDate, MaxHours
+FROM Project
+WHERE MaxHours >
+ (SELECT AVG(MaxHours)
+ FROM project
+ WHERE StartDate=StartDate)
+ORDER BY StartDate, MaxHours
+
+2017-10-05       
+NULL
+140.00
+
+2017-08-10
+2017-10-15
+145.00
+
+2017-08-10
+2017-09-15
+135.00
+
+2017-05-10
+2017-06-15
+135.00
